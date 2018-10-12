603e4e30257cf75a4716ff515d993574235bcddb
Saint-Jean Germila <germila@noukod.com>
2018-10-12 17:28:00 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,11 +1,11 @@
 class Cat
 
-attr_reader :name
-attr_writer :mood
-def initialize(name, mood)
-    @name = name
-    @mood = "nervous"
-  end
-end
-
   # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+    def initialize(name)
+      @name = name
+      @mood = "nervous"
+    end
+end
